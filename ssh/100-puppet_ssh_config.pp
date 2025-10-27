69c28509695b4bb2badd6014465a881dea87db65
shyakafrancis123 <s.francis@alustudent.com>
2025-10-27 05:16:48 -0700

    hh

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,19 @@
-# 100-puppet_ssh_config.pp
-# Puppet manifest to configure SSH client for passwordless login using a private key
-
-# Ensure the system-wide SSH client config file exists
 file { '/etc/ssh/ssh_config':
-  ensure => file,
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
 }
 
-# Declare the identity file (private key)
 file_line { 'Declare identity file':
   path  => '/etc/ssh/ssh_config',
-  line  => 'IdentityFile ~/.ssh/school',
+  line  => '    IdentityFile ~/.ssh/school',
+  match => '^    IdentityFile',
 }
 
-# Turn off password authentication
 file_line { 'Turn off passwd auth':
   path  => '/etc/ssh/ssh_config',
-  line  => 'PasswordAuthentication no',
+  line  => '    PasswordAuthentication no',
+  match => '^    PasswordAuthentication',
 }
 
